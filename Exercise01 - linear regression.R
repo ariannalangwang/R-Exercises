06935ea8e475c9b794c590a275ce5b0f98a974bd
Arianna Lang Wang <32724597+ariannalangwang@users.noreply.github.com>
2017-11-08 15:36:06 -0800

    Add files via upload

--- Exercise01 - linear regression.R
+++ Exercise01 - linear regression.R
@@ -1,63 +1,75 @@
 ###########################
 #### LINEAR REGRESSION ####
 ###########################
-
-#### Get our Data 
+####
+## Get our Data 
+####
 library(readr)
+# Read the file in using R-Studio
 df <- read_delim("~/Documents/****SELF-STUDY****/Udemy-Data Science and Machine Learning Bootcamp with R - Jose Portilla/R-for-Data-Science-and-Machine-Learning/Machine Learning with R/student-mat.csv", 
                    +     ";", escape_double = FALSE, trim_ws = TRUE)
 head(df)
 tail(df)
 str(df)
 summary(df)
- 
-#### Clean the Data  
-any(is.na(df)) # is.na(object) returns a vector or amatrix.
+
+####
+## Clean the Data  
+####
+any(is.na(df)) # is.na(object) returns a vector or a matrix.
 # False
 # class(is.na(df))  # matrix
- 
-#### Exploratory Data Analysis  
-## Correlation and CorrPlots
 
-# Grab only numeric columns
-num.cols <- sapply(df, is.numeric)  # num.cols is a vector of T and F
+####
+## Exploratory Data Analysis  
+####
 
-# Filter to numeric columns for correlation
-cor.data <- cor(df[ ,num.cols])
+# correlation
+num.cols <- sapply(df, is.numeric)  # Grab only numeric columns
+num.cols  # num.cols is a vector of TRUE and FALSE
+df[num.cols] # new dataframe with only numeric columns
+cor.data <- cor(df[num.cols])
 
 # corrplot
+install.packages('corrplot')
 library(corrplot)
 corrplot(cor.data, method='color')
 
+# corrgram (corrgram allows passing in the dataframe directly)
+install.packages('corrgram') 
+library(corrgram)
+corrgram(df)
+
 # We're trying to predict the G3 score. So let's see a histogram of these scores:
 library(tidyverse)
-ggplot(df,aes(x=G3)) + geom_histogram(bins=20,alpha=0.5,fill='blue') + theme_minimal()
+ggplot(df, aes(x=G3)) + geom_histogram(bins=20, alpha=0.5, fill='blue') + theme_minimal()
 
-#### Train and Test Data
+####
+## Train and Test Data
+####
 library(caTools)
 set.seed(101) 
-sample <- sample.split(df$G3, SplitRatio = 0.70) # df$G3: can choose any column. Choose the y column by convention.
+sample <- sample.split(df$G3, SplitRatio = 0.70)  # df$G3: can choose any column. Choose the y column by convention.
 train = subset(df, sample == TRUE)
 test = subset(df, sample == FALSE)
 
-#### Train our Model
+# Train our Model
 model <- lm(G3 ~ ., data=train)
 summary(model)
+
 names(model)  # see other functions of the model
 # for example, compute fitted values of the model
 fitted.values(model)
- 
-# plot the model
-plot(G3 ~ ., data=train)
-abline(model, col='red')
+residuals(model)
 
 # use ggplot to plot the model
-ggplot(train, aes(x = train$absences, y = train$G3)) + 
-  geom_point() +
-  stat_smooth(method = "lm", col = "red")
+plt1 <- ggplot(data=train, aes(x = train$absences, y = train$G3)) + geom_point() 
+plt2 <- plt1 + stat_smooth(method = "lm", col = "red")
+print(plt2)
 
 # Grab residuals
 res <- residuals(model)
+
 # Convert to DataFrame for gglpot
 res <- as.data.frame(res)
 head(res)
@@ -65,18 +77,20 @@ head(res)
 # We want a histogram of our residuals to be normally distributed, 
 # something with a strong bimodal distribution may be a warning that 
 # our data was not a good fit for lienar regression.
-ggplot(res, aes(res)) +  geom_histogram(fill='blue',alpha=0.5)
+ggplot(data=res, aes(x=res)) +  geom_histogram(fill='blue',alpha=0.5)
 
 # Looks like there are some suspicious residual values that have a value less than -5. 
 # We can further explore this by just calling plot on our model.
+# Regression Validation - four-plots to see the fit of the model:
+plot(model)
 
-#### Predictions
-## test our model by predicting on our testing set.
-G3.predictions <- predict(model, data=test)  # gives all the y_hat valeus in the test set.
+# Predictions
+# test our model by predicting on our testing set.
+G3_predictions <- predict(model, data=test)  # gives all the y_hat valeus in the test set.
 
 # Now we can get the root mean squared error (square root of MSE), 
 # a standardized measure of how off we were with our predicted values.
-results <- cbind(G3.predictions,test$G3) 
+results <- cbind(G3_predictions, test$G3) 
 colnames(results) <- c('pred','real')
 results <- as.data.frame(results)
 
@@ -99,7 +113,7 @@ print(MSE)
 SSE <- sum((results$real-results$pred)^2)
 SST <- sum((results$real-mean(test$G3))^2) 
 R2 <- 1 - SSE/SST
-R2
+print(R2)
 
 
 
