71426303577bab5baea288961d3bc4d64a67be39
Arianna Lang Wang <32724597+ariannalangwang@users.noreply.github.com>
2017-11-10 10:12:54 -0800

    Add files via upload

--- Exercise05 - K nearest neighbors.R
+++ Exercise05 - K nearest neighbors.R
@@ -1,32 +1,53 @@
-#########################
-## K Nearest Neighbors ##
-#########################
-#### Get Data
+####################################
+## K Nearest Neighbors Classifier ##
+####################################
+
+####
+## Get Data
+####
 # We'll use the ISLR package to get the data
 install.packages('ISLR')
 library(ISLR)
+
 # We will apply the KNN approach to the Caravan data set. 
 # This data set has 85 predictors that measure characteristics for 5,822 individuals. 
+head(Caravan)
+str(Caravan)
+
 # The response variable is Purchase, which indicates whether or not a given 
 # individual purchases a Caravan insurance policy. 
 # In this data set, only 6% of people purchased caravan insurance.
-head(Caravan)
-str(Caravan)
-any(is.na(Caravan))  # False
+summary(Caravan$Purchase)
+
 ####
-#### Standardize Variables
+## Cleaning Data
+####
+
+any(is.na(Caravan))  
+# False
+
+
+####
+## Standardize Variables
+####
 # The KNN classifier predicts the class of a test observation 
 # by identifying the observations that are nearest to it, 
 # so, the scale of the variables matters. 
-standardized.Caravan <- scale(Caravan[,-86])
-purchase <- Caravan[,86] # Note: this is a vector
+# Any variables that are on a large scale will have a much larger effect 
+# on the distance between the observations, and hence on the KNN classifier, 
+# than variables that are on a small scale.
+
+standardized.Caravan <- scale(Caravan[ ,-86])  # -86, the purchase column.
+purchase <- Caravan[ ,86]  # Note: this is a vector
+
 # Let's check the variance:
-var(standardized.Caravan[,1])
+var(standardized.Caravan[ ,1])
+# 1
 
 ####
-#### Training & Testing Data
+## Training & Testing Data
 ####
-# First 100 rows as test data:
+# First 1000 rows as test data:
 test.index <- 1:1000
 test.data <- standardized.Caravan[test.index, ]
 test.purchase <- purchase[test.index]
@@ -34,10 +55,17 @@ train.data <- standardized.Caravan[-test.index, ]
 train.purchase <- purchase[-test.index]
 
 ####
-#### Using KNN
-## knn() function returns a vector of predicted Y’s.
+## Using KNN
+####
+
+# knn() function returns a vector of predicted Y’s.
+# The first argument is the training data  
+# the second argument is the testing data  
+# the third argument is the training Y 
+# the fourth argument is the k (how many neighbors).
+
 library(class)
-predicted.purchase <- knn(train.data,test.data,train.purchase,k=1)
+predicted.purchase <- knn(train.data, test.data, train.purchase, k=1)
 head(predicted.purchase)
 
 # misclassification rate:
@@ -46,23 +74,27 @@ error.rate <- mean(predicted.purchase != test.purchase)
 ####
 ## Choosing K Value:
 ####
-predicted.purchase <- NULL
+
+# Write a for loop to check misclassification rate with k-value 1-20:
+predicted.purchase <- NULL  # empty object
 error.rate <- NULL
 for(i in 1:20){
   predicted.purchase <- knn(train.data,test.data,train.purchase,k=i)
   error.rate[i] <- mean(predicted.purchase != test.purchase)
 }
+
 error.rate
 
-## Elbow Method
+# Elbow Method
 # We can plot out the various error rates for the K values. 
 # We should see an "elbow" indicating that we don't get a decrease 
-# in error rate for using a higher K. This is a good cut-off point:
+# in error rate for using a higher K. This is the good cut-off point.
+
 k.values <- 1:20
 error.df <- data.frame(error.rate, k.values)
-ggplot(error.df, aes(x=k.values,y=error.rate)) + geom_point()+ geom_line(lty="dotted",color='red')
-# beyond K=9 does not help our misclassification at all. 
-# So we can set K=9 for our model during training.
+ggplot(error.df, aes(x=k.values, y=error.rate)) + geom_point() + geom_line(lty="dotted", color='red')
+# From the graph, we can see that beyond K=9 does not help the misclassification rate 
+# at all. So we can set K=9 for our model during training.
 
 
 
